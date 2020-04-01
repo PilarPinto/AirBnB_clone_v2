8b4334ef59258d4a9b6af921979a4c735572705b
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-01 00:08:24 +0000

    [Update]: Adding a conditional depending of the value of the environment variable HBNB_TYPE_STORAGE

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
+import os
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
+from models.engine.db_storage import DBStorage
+from models.base_model import BaseModel, Base
 from models.user import User
 from models.state import State
 from models.city import City
@@ -10,5 +12,8 @@ from models.place import Place
 from models.review import Review
 
 
-storage = FileStorage()
+if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
