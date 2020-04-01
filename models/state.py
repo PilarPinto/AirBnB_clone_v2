d0a0b959ab936a45503c76550b1984bcf6af8678
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-01 18:28:22 +0000

    [Fix]: Typo

--- models/state.py
+++ models/state.py
@@ -13,10 +13,10 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete,"
-                          " delete-orphan")
+    cities = relationship("City", cascade="all, delete, delete-orphan",
+                          backref="state")
 
     @property
     def cities(self):
