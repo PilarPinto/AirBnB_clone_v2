21b4f90aa7cb096aadc6bd58b21de46ce6667acc
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-01 00:06:31 +0000

    [Update]: Adding tablename attribute

--- models/state.py
+++ models/state.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+import models
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    @property
+    def cities(self):
+        """Return:
+        The list of City instances with state_id equals
+        to the current State.id
+        """
+        objs = models.storage.all(City)
+        return [v for k, v in objs.items() if v.state_id == self.id]
