4863dede68a70541b6bc23f5b9c9f62d5b3c82da
PilarPinto <piapintoch@unal.edu.co>
2020-04-01 00:43:07 +0000

    [Fix]: Deleting import State

--- models/city.py
+++ models/city.py
@@ -5,7 +5,6 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from models.place import Place
-from models.state import State
 
 
 class City(BaseModel, Base):
