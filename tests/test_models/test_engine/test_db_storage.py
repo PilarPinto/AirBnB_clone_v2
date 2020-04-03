dde1541b92d17f75e8f8a9bc0e4c9db32c0c761e
PilarPinto <piapintoch@unal.edu.co>
2020-04-03 03:50:45 +0000

    [Fix]: Removing duplicate methods and test db storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -23,13 +23,3 @@ class TestDBStorage(unittest.TestCase):
         '''Test existence of database'''
         pass
 
-    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_pep8_FileStorage(self):
-        """Tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
-
-if __name__ == "__main__":
-    unittest.main()
