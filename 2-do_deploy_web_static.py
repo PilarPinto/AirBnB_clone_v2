9121a59fe7ef2f5c57d6e328be69f7661fd0cfa7
PilarPinto <piapintoch@unal.edu.co>
2020-04-09 19:19:30 +0000

    [Feature]:Fabric script that distributes an archive to your web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that generates a .tgz archive from the contents of the web_static
+"""
+import os
+from fabric.api import env, run, put
+import datetime
+
+env.hosts = ['54.237.196.98', '104.196.27.193']
+env.user = 'ubuntu'
+
+def do_deploy(archive_path):
+    if not os.path.exists(archive_path):
+        return False
+
+    file_name = archive_path.split('/')
+    new_name = file_name[1]
+    new_var = new_name.split('.')
+    path_tar ="/data/web_static/releases/{}/".format(new_var[0])
+    link_cur = "/data/web_static/current"
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_tar))
+        run("tar -xzf /tmp/{} -C {}".format(new_name, path_tar))
+        run("rm /tmp/{}".format(new_name))
+        run("mv {}web_static/* {}".format(path_tar, path_tar))
+        run("rm -rf {}".format(link_cur))
+        run("ln -s {} {}".format(path_tar, link_cur))
+        return True
+    except:
+        return False
