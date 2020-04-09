c38eba9c7e30c41273f28e269b9ec52f1d9c48f0
PilarPinto <piapintoch@unal.edu.co>
2020-04-09 19:25:46 +0000

    [Fix]:Pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,7 @@ import datetime
 env.hosts = ['54.237.196.98', '104.196.27.193']
 env.user = 'ubuntu'
 
+
 def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
@@ -16,7 +17,7 @@ def do_deploy(archive_path):
     file_name = archive_path.split('/')
     new_name = file_name[1]
     new_var = new_name.split('.')
-    path_tar ="/data/web_static/releases/{}/".format(new_var[0])
+    path_tar = "/data/web_static/releases/{}/".format(new_var[0])
     link_cur = "/data/web_static/current"
     try:
         put(archive_path, "/tmp/")
