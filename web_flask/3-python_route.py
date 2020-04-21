0ca7d81598bb4cd42b26185fbe07304b69f7bbab
PilarPinto <piapintoch@unal.edu.co>
2020-04-21 19:39:53 +0000

    [Feature]:Display using default text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from flask import Flask, escape
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_flask():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def display_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def display_cisfun(text):
+    return 'C %s' % escape(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def display_default(text):
+    return 'Python is %s' % escape(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
