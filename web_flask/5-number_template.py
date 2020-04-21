8a19ab4e4ba22d3c2881bbecd573b0ea4c2640ac
PilarPinto <piapintoch@unal.edu.co>
2020-04-21 19:53:40 +0000

    [Feature]:Display using template HTML

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+from flask import Flask, escape, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_flask():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def display_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def display_cisfun(text):
+    return 'C %s' % escape(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def display_default(text):
+    return 'Python %s' % escape(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def display_integer(n):
+    return '%i is a number' % (n)
+
+
+@app.route('/number_template/<int:n>')
+def display_Template(n):
+    '''display template from flask'''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
